2cd9fae8d76a5321b31b53879f9e714a328c44f0
yinzhedy <yinzhe.dy@gmail.com>
2021-11-22 20:53:47 -0500

    filled out template portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Yinzhe Dong-Ydeen",
+    "role": "Full stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/yinzhe-dong-ydeen-7a116121b/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I'm a recent graduate of Upenn's full-time fullstack coding bootcamp. Aspiring game developer with a passion for art and all things creative.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/yinzhe-dong-ydeen-7a116121b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/yinzhedy",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I've been passionate about video games since I was a little girl. My first dream job was to work for Square Enix, creating characters for the Final Fantasy series! My passion for art, love of video games, and need for creativity is what drove me to pursue a career as a developer. The sole motivation behind my work is to create; to one day be able to offer people memorable and treasured experiences, akin to the ones I was lucky enough to have as a child - through games.",
+    "address":"Easton, Pennsylvania, United States",
+    "email" : "yinzhe.dy@gmail.com",
+    "website":"https://yinzhedy.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Pennsylvania Coding Bootcamp",
+        "specialization":"Full Stack - Full Time",
+        "MonthOfPassing":"November",
+        "YearOfPassing":"2021",
+        "Achievements":"Certfication"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Arts Du Monde",
+        "specialization":"Assistant",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2021",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Heroku"
+      },
+      {
+        "skillname":"GraphQL"
+      },
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Mongoose"
+      },
+      {
+        "skillname":"Node"
+      },
+      {
+        "skillname":"ES6"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"Express_Handlebars"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Shadow Chaser",
+        "description":"MERN stack application. Javascript, React, Express, Apollo, GraphQL, Heroku",
+        "imgurl":"images/screenshot.png",
+        "href" : "https://shadow-chaser-an-arcade-game.herokuapp.com/"
+      },
+      {
+        "name":"Date Night",
+        "description":"Javascript, HTML5, CSS",
+        "imgurl":"images/date-night.jpg",
+        "href": "https://henlowgg.github.io/Date-Night/"
+      },
+      {
+        "name":"Budget Tracker",
+        "description":"MongoDB, Express, Javascript, HTML5, CSS",  
+        "imgurl":"images/budget-tracker.png",
+        "href" : "https://limitless-fjord-00031.herokuapp.com/"
+      },
+      {
+        "name":"Fitness Tracker",
+        "description":"MongoDB, Express, Javascript, HTML5, CSS",
+        "imgurl":"images/fitness-tracker.png",
+        "href": "https://serene-meadow-79906.herokuapp.com/"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
